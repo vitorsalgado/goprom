f15ce9659d1aa6ba27535c21c5bc274fd258c8d9
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-06-03 17:08:56 -0400

    test: add end-to-end for the main workflow

--- test/e2e/promo_feed_retrieve_test.go
+++ test/e2e/promo_feed_retrieve_test.go
@@ -1,23 +1,107 @@
 package e2e
 
 import (
-	"github.com/go-redis/redis/v8"
-	"github.com/vitorsalgado/goprom/internal/std/config"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/vitorsalgado/goprom/internal/domain"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"path"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestMain(m *testing.M) {
-	conf := config.Load()
-	conf.RedisAddr = "localhost:6379"
+func TestFlow(t *testing.T) {
+	wd, _ := os.Getwd()
+	td := path.Join(wd, "_testdata")
+	promotions := path.Join(wd, "_testdata", "promotions.csv")
 
-	client := ConnectToRedis(conf, 20*time.Second)
-	defer func(c *redis.Client) {
-		_ = c.Close()
-	}(client)
+	f, err := os.Create(promotions)
+	if err != nil {
+		log.Fatalf("failed to create promotions file %s", promotions)
+	}
 
-	c := m.Run()
+	defer f.Close()
 
-	os.Exit(c)
+	w := bufio.NewWriter(f)
+
+	_, _ = w.WriteString("d018ef0b-dbd9-48f1-ac1a-eb4d90e57118,60.683466,2018-08-04 05:32:31 +0200 CEST\n")
+	_, _ = w.WriteString("e2649ca5-7e05-4d53-a8ff-919917a4922e,66.640497,2018-08-22 18:34:11 +0200 CEST")
+	_ = w.Flush()
+
+	id := "d018ef0b-dbd9-48f1-ac1a-eb4d90e57118"
+	ticker := time.NewTicker(10 * time.Second)
+	timeout := time.After(2 * time.Minute)
+	ch := make(chan bool)
+
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Println("checking if promotions were imported ...")
+				files, err := ioutil.ReadDir(td)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if len(files) > 2 {
+					// assume that the process has ended
+					ch <- true
+					return
+				}
+
+			case <-timeout:
+				ch <- false
+				return
+			}
+
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	c := <-ch
+	if !c {
+		log.Fatal("could ensure that the promotions were imported")
+	}
+
+	log.Println("promotions were imported. continuing with tests")
+
+	res, err := http.Get(fmt.Sprintf("http://localhost:8080/promotions/%s", id))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	promo := &domain.Promotion{}
+	err = json.NewDecoder(res.Body).Decode(promo)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Equal(t, promo.ID, id)
+	assert.Equal(t, promo.Price, 60.68)
+	assert.Equal(t, promo.ExpirationDate, "2018-08-04 05:32:31")
+
+	files, err := ioutil.ReadDir(td)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() && strings.Contains(file.Name(), ".txt") || strings.Contains(file.Name(), "--imported") {
+			err = os.Remove(path.Join(td, file.Name()))
+			if err != nil {
+				log.Fatalf("error removing test data file %s. reason: %s", file.Name(), err.Error())
+			}
+		}
+	}
+
+	log.Println("finished testing main workflow")
 }
--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 function clean() {
-  docker-compose -f "$DOCKER_COMPOSE_ROOT/base.yml" down --volumes --remove-orphans || true
-  docker-compose -f "$DOCKER_COMPOSE_ROOT/base.yml" rm || true
+  docker-compose -f "test/e2e/docker-compose.yml" down || true
 }
 
 trap clean EXIT
 
-docker-compose -f "$DOCKER_COMPOSE_ROOT/base.yml" up -d --build --remove-orphans
-
-echo "Waiting to ensure everything is online before tests"
-go run ./test/e2e/cmd/ping/main.go
+docker-compose -f "test/e2e/docker-compose.yml" up -d
 
 echo "Run all integration and e2e tests"
 go test -v ./test/...
